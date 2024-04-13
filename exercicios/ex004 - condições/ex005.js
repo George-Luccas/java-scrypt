c482003fca9f5b363898b9ab3481384cbfa621ba
George-Luccas <georgeluccas300@gmail.com>
2024-04-13 14:18:38 -0400

    atualizando curso java scrpt

---
+++ exercicios/ex004 - condições/ex005.js
@@ -0,0 +1,8 @@
+pais = 'brasil'
+console.log(`Você mora em ${pais}`)
+if(pais != 'brasil'){
+    console.log('Você é estrangeiro')
+}
+else { 
+    console.log('Você é Brasileiro')
+}
---
+++ exercicios/ex004 - condições/exercicio004.js
@@ -0,0 +1,6 @@
+var vel = 140
+console.log(`Sua velocidade é de ${vel}Km/Hr` )
+if(vel >= 100) {
+    console.log('Voce esta acima da velocidade permitida! Multado')}
+
+    console.log('Boa Viage , use cinto de segurança')
